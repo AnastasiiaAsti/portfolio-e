63908250364999d27839631f9438656c4cdcf215
Asti <astistateofmind@gmail.com>
2023-02-08 18:48:32 -0800

    adds shadows to table

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,5 @@
+// real life clock logic
+
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
