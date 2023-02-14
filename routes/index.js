e360f2b6b744104393b8b39e9a8724f472539b81
Asti <astistateofmind@gmail.com>
2023-02-13 16:48:27 -0800

    sets board for portfolio projects

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const Project = require('../models/project');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  Project.find({}, function (err, projects) {
+    res.render('index', {projects});
+  })
 });
 
 module.exports = router;
